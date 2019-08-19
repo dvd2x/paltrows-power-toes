2000a5a9564d758a6af454d9c583571a8940aa23
David Piper <black.god1930@gmail.com>
2019-08-19 12:32:59 -0400

    Updates

---
+++ precache-manifest.1a1b065f813ad1ea3f2541c210db840e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed61ec83c8a6da42dbcd",
+    "url": "/paltrows-power-toes/static/css/main.543c8046.chunk.css"
+  },
+  {
+    "revision": "ed61ec83c8a6da42dbcd",
+    "url": "/paltrows-power-toes/static/js/main.ed61ec83.chunk.js"
+  },
+  {
+    "revision": "726cb51e5e4550bfc03d",
+    "url": "/paltrows-power-toes/static/js/1.726cb51e.chunk.js"
+  },
+  {
+    "revision": "48d14e7cbe0565f6d4ef",
+    "url": "/paltrows-power-toes/static/js/runtime~main.48d14e7c.js"
+  },
+  {
+    "revision": "af6bdd6c06b813d5b5c77f5544f01c52",
+    "url": "/paltrows-power-toes/index.html"
+  }
+];
